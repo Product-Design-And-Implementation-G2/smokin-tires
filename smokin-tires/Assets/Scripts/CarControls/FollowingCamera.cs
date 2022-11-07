8cbb5fad96007aa01b961891578d17d43a41f34c
t9musa <t9musa00@students.oamk.fi>
2022-11-07 11:54:01 +0200

    SandDunesMap
    Added:
    -improved camera controls
    -new map
    -concept game + amazing racer
    -some textures

---
+++ smokin-tires/Assets/Scripts/CarControls/FollowingCamera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    public Transform Player;
+    public Vector3 CameraOffSet;
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.position = Player.position +CameraOffSet;
+    }
+}
