4f2f24833cd5ab8964cfba77b45d4e07a4d98bfa
t9musa <t9musa00@students.oamk.fi>
2022-11-18 12:00:08 +0200

    ai_scripts_added

---
+++ smokin-tires/Assets/Scripts/AI/CarAiHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAiHandler : MonoBehaviour
+{
+    PrometeoCarController carController;
+
+    //Awake is called when the script isntance is being loaded.
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
